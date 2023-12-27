56c849dc1694acd08f373aa6fa227d5e7a3d1b0a
HiryKun <1951086367@qq.com>
2023-12-27 20:42:36 +0800

    ST7789 Demo

--- keyboards/lcd_test/keymaps/default/keymap.c
+++ keyboards/lcd_test/keymaps/default/keymap.c
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "1_qgf.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -22,3 +23,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
     )
 };
+
+void keyboard_post_init_kb(void) {
+    painter_device_t display = qp_st7735_make_spi_device(128, 160, A4, B1, B0, 4, 0);
+    qp_init(display, QP_ROTATION_180);
+    qp_set_viewport_offsets(display, 2, 1);
+    qp_power(display, true);
+    qp_clear(display);
+    painter_image_handle_t image1 = qp_load_image_mem(gfx_____1);
+    qp_drawimage(display, 0, 0, image1);
+    qp_flush(display);
+}
